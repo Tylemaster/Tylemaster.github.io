8c2a169c8cb819e054fddfc1098b90b4e035fa39
Tylemaster <tylerstrang@hotmail.com>
2021-11-02 14:31:54 -0230

    Commiting progress to work on another comp

---
+++ willowGame.js
@@ -0,0 +1,9 @@
+﻿ var willowGameText = document.getElementById("theTextid");
+ var xmlhttp = new XMLHttpRequest();
+ xmlhttp.open("GET", "text.txt", false);
+ xmlhttp.send();
+ var theThing = xmlhttp.responseText;
+ var thingArray= theThing.split(/\r?\n/);
+ function willowGameUpdate() {
+    willowGameText.innerText = thingArray[0];
+}
