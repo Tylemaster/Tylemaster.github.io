b6fcaeb98d08da76a95510ebe4131a0cab27264b
Tyler Strang <tylerstrang@hotmail.com>
2021-11-03 13:45:23 -0230

    Most of the functionality and styling is working

--- willowGame.js
+++ willowGame.js
@@ -1,9 +1,29 @@
-﻿ var willowGameText = document.getElementById("theTextid");
- var xmlhttp = new XMLHttpRequest();
- xmlhttp.open("GET", "text.txt", false);
- xmlhttp.send();
- var theThing = xmlhttp.responseText;
- var thingArray= theThing.split(/\r?\n/);
- function willowGameUpdate() {
-    willowGameText.innerText = thingArray[0];
+﻿var willowGameText = document.getElementById("theTextid");
+var highScoreElement = document.getElementById("highScoresid");
+var currentHighScore = document.getElementById("currentScore");
+var buttonElement = document.getElementById("theButtonId");
+
+var winCount = 0
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "text.txt", false);
+xmlhttp.send();
+var gameText = xmlhttp.responseText;
+var textArray = gameText.split(/\r?\n/);
+
+function willowGameUpdate() {
+    currentHighScore.innerHTML = "you - ".concat(winCount);
+    if(textArray[winCount].substring(0,2) == "~1"){
+//add code here for alert button
+    }
+    else if(textArray[winCount].substring(0,2) == "~2"){
+        highScoreElement.style.visibility = "visible"
+        textArray.splice(winCount,1);
+    }
+    else if(textArray[winCount].substring(0,2) == "~3"){
+        buttonElement.style.visibility = "hidden"
+        textArray.splice(winCount,1);
+    }
+    willowGameText.innerText = textArray[winCount];
+    winCount++;
 }
