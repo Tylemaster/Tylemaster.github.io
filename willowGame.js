0e0cb1cf6ab5ca03fd2d80156a67eedce0049c33
Tylemaster <tylerstrang@hotmail.com>
2021-11-03 21:27:08 -0230

    finished the 'game' part and added simple comments. done unless i want to add something

--- willowGame.js
+++ willowGame.js
@@ -1,20 +1,25 @@
-﻿var willowGameText = document.getElementById("theTextid");
+﻿//grabbing elements from html doc
+var willowGameText = document.getElementById("theTextid");
 var highScoreElement = document.getElementById("highScoresid");
 var currentHighScore = document.getElementById("currentScore");
 var buttonElement = document.getElementById("theButtonId");
 
-var winCount = 0
-
+//getting acces to text file where the storey of the game is stored
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "text.txt", false);
 xmlhttp.send();
 var gameText = xmlhttp.responseText;
 var textArray = gameText.split(/\r?\n/);
 
+var winCount = 0
 function willowGameUpdate() {
-    currentHighScore.innerHTML = "you - ".concat(winCount);
+    //update the high score of the game (hidden until enough button presses)
+    currentHighScore.innerHTML = "you - ".concat(winCount + 1);
+    //cases that change something about the page (reveal high score, hide button etc.)
+    //also removes the special character so it is not printed 
     if(textArray[winCount].substring(0,2) == "~1"){
-//add code here for alert button
+        window.alert(" uh oh, heres a curve ball! Try to hit that button!")
+        textArray.splice(winCount,1);
     }
     else if(textArray[winCount].substring(0,2) == "~2"){
         highScoreElement.style.visibility = "visible"
@@ -24,6 +29,7 @@ function willowGameUpdate() {
         buttonElement.style.visibility = "hidden"
         textArray.splice(winCount,1);
     }
+    //update the txt on every button press
     willowGameText.innerText = textArray[winCount];
     winCount++;
 }
